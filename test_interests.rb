4033b9375686b9110a5360991e547b637cd95b57
kai <kai@kaivong.com>
2022-07-25 09:01:38 +0100

    edit test

--- test_interests.rb
+++ test_interests.rb
@@ -15,27 +15,20 @@ class XMLObjectifierTest < Test::Unit::TestCase
 		@xml = XML_Objectifier.new(xml) end
 
 	def test_arrays
-		actual = @xml.to_a
-		expects = %w{cats penguins cats penguins dogs}
-		assert_equal(expects, actual)
-	end
+		actual, expects = @xml.to_a, %w{cats penguins cats penguins dogs}
+		assert_equal(expects, actual) end
 
 	def test_counting
-		actual = @xml.count
-		expects = 5
-		assert_equal(expects, actual)
-	end
+		actual, expects = @xml.count, 5
+		assert_equal(expects, actual) end
 
 	def test_hashes
 		actual = @xml.to_hash
 		expects = [{:name=>"cats"}, {:name=>"penguins"},{:name=>"cats"}, {:name=>"penguins"}, {:name=>"dogs"}]
-		assert_equal(expects, actual)
-	end
+		assert_equal(expects, actual) end
 
 	def test_date_information_xml_attributes
 		actual = @xml.date_information
 		expects = [{ :date => '29/6/2022' }, { :date => '1/7/2022' } ]
-		assert_equal(expects, actual)
-	end
-
+		assert_equal(expects, actual) end
 end
