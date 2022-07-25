a6ffccd7cd945045343a97816386320a4a8bbee1
kai <kai@kaivong.com>
2022-07-25 09:01:04 +0100

    edit test setup

--- test_interests.rb
+++ test_interests.rb
@@ -3,29 +3,37 @@ require "./xml_objectifier"
 
 class XMLObjectifierTest < Test::Unit::TestCase
 	def setup
-		@xml = open('interests.xml').read
-	end
+		xml = '<interests date="29/6/2022">
+					<interest name="cats"></interest>
+					<interest name="penguins"></interest>
+				</interests>
+				<interests date="1/7/2022">
+					<interest name="cats"></interest>
+					<interest name="penguins"></interest>
+					<interest name="dogs"></interest>
+				</interests>'
+		@xml = XML_Objectifier.new(xml) end
 
 	def test_arrays
-		actual = XML_Objectifier.new(@xml).to_a
+		actual = @xml.to_a
 		expects = %w{cats penguins cats penguins dogs}
 		assert_equal(expects, actual)
 	end
 
 	def test_counting
-		actual = XML_Objectifier.new(@xml).count
+		actual = @xml.count
 		expects = 5
 		assert_equal(expects, actual)
 	end
 
 	def test_hashes
-		actual = XML_Objectifier.new(@xml).to_hash
+		actual = @xml.to_hash
 		expects = [{:name=>"cats"}, {:name=>"penguins"},{:name=>"cats"}, {:name=>"penguins"}, {:name=>"dogs"}]
 		assert_equal(expects, actual)
 	end
 
 	def test_date_information_xml_attributes
-		actual = XML_Objectifier.new(@xml).date_information
+		actual = @xml.date_information
 		expects = [{ :date => '29/6/2022' }, { :date => '1/7/2022' } ]
 		assert_equal(expects, actual)
 	end
